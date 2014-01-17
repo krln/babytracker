9c129a6150c63af8bc298bae70a87e952dfeea23
krln <krlnkmsk88@gmail.com>
2014-01-17 19:31:37 +0100

    pierwsza wersja

---
+++ babytracker.rb
@@ -0,0 +1,35 @@
+# encoding: UTF-8
+
+Serving = Struct.new(:amount, :name)
+
+require 'sinatra'
+
+SERVINGS = [
+	Serving.new(
+		50,
+		'Marchewka'
+		),
+	Serving.new(
+		100,
+		'Mleko sztuczne'
+		),
+	Serving.new(
+		2,
+		'Jabłko'
+		)
+]
+
+get '/' do 
+	@servings = SERVINGS
+	erb :index
+end
+
+post '/create' do
+	params.inspect
+	new_serving = Serving.new(
+		params["number"].to_i,
+		params["name"]
+		)
+	SERVINGS << new_serving
+	redirect "/"
+end
