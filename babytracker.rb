c6eb0b55fbb09ebfd4b78ab527eb28ea871e52ac
krln <krlnkmsk88@gmail.com>
2014-01-28 15:35:29 +0100

    dodanie select

--- babytracker.rb
+++ babytracker.rb
@@ -1,35 +1,35 @@
 # encoding: UTF-8
 
-Serving = Struct.new(:amount, :name)
+Product = Struct.new(:amount, :name)
 
 require 'sinatra'
 
-SERVINGS = [
-	Serving.new(
+PRODUCTS = [
+	Product.new(
 		50,
 		'Marchewka'
 	),
-	Serving.new(
+	Product.new(
 		100,
 		'Mleko sztuczne'
 	),
-	Serving.new(
+	Product.new(
 		2,
 		'Jabłko'
 	)
 ]
 
 get '/' do 
-	@servings = SERVINGS
+	@product = PRODUCTS
 	erb :index
 end
 
 post '/create' do
 	params.inspect
-	new_serving = Serving.new(
+	new_product = Product.new(
 		params["number"].to_i,
 		params["name"]
 		)
-	SERVINGS << new_serving
+	PRODUCT << new_product
 	redirect "/"
 end
